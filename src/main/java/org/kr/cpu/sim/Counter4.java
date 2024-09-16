f5e61cb5849e99cb8e020a1df16cd67771a5658c
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-09-16 15:18:21 +0200

    implement counter4

---
+++ src/main/java/org/kr/cpu/sim/Counter4.java
@@ -0,0 +1,47 @@
+package org.kr.cpu.sim;
+
+public class Counter4 extends Component {
+    static final InputPin PIN_CLK = new InputPin("CLK", 0);
+    static final OutputPin[] PIN_Q = new OutputPin[] {
+            new OutputPin("Q", 0), new OutputPin("Q", 1),
+            new OutputPin("Q", 2), new OutputPin("Q", 3)};
+
+    static final OutputPin PIN_C = new OutputPin("C", 4);
+
+    Counter4(String id) {
+        super(id, new boolean[1], new boolean[5]);
+    }
+
+    private DFlipFlop[] state = new DFlipFlop[] {
+            new DFlipFlop("FF1"), new DFlipFlop("FF2"), new DFlipFlop("FF3"), new DFlipFlop("FF4")
+    };
+
+    private void updateOutput() {
+        output[PIN_Q[0].order] = state[0].output[DFlipFlop.PIN_Q.order];
+        output[PIN_Q[1].order] = state[1].output[DFlipFlop.PIN_Q.order];
+        output[PIN_Q[2].order] = state[2].output[DFlipFlop.PIN_Q.order];
+        output[PIN_Q[3].order] = state[3].output[DFlipFlop.PIN_Q.order];
+        output[PIN_C.order] = state[3].output[DFlipFlop.PIN_nQ.order];
+    }
+
+    @Override
+    Component setInput(InputPin pin, boolean value) {
+        assert pin == PIN_CLK;
+
+        state[0]
+                .setInput(DFlipFlop.PIN_D, state[0].output[DFlipFlop.PIN_nQ.order])
+                .setInput(DFlipFlop.PIN_CLK, value);
+        state[1]
+                .setInput(DFlipFlop.PIN_D, state[1].output[DFlipFlop.PIN_nQ.order])
+                .setInput(DFlipFlop.PIN_CLK, state[0].output[DFlipFlop.PIN_nQ.order]);
+        state[2]
+                .setInput(DFlipFlop.PIN_D, state[2].output[DFlipFlop.PIN_nQ.order])
+                .setInput(DFlipFlop.PIN_CLK, state[1].output[DFlipFlop.PIN_nQ.order]);
+        state[3]
+                .setInput(DFlipFlop.PIN_D, state[3].output[DFlipFlop.PIN_nQ.order])
+                .setInput(DFlipFlop.PIN_CLK, state[2].output[DFlipFlop.PIN_nQ.order]);
+        input[pin.order] = value;
+        updateOutput();
+        return this;
+    }
+}
--- src/main/java/org/kr/cpu/sim/Main.java
+++ src/main/java/org/kr/cpu/sim/Main.java
@@ -1,5 +1,7 @@
 package org.kr.cpu.sim;
 
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("START");
@@ -16,6 +18,23 @@ public class Main {
         System.out.println(df.fullTxt());
         df.setInput(DFlipFlop.PIN_CLK, true);
         System.out.println(df.fullTxt());
+
+        System.out.println("------------------");
+
+        Counter4 counter = new Counter4("CNT1");
+
+        System.out.println(LocalDateTime.now());
+        int max = 100000000;
+        int print = 2;
+        for(int i = 0; i < max; i++) {
+            boolean clk = i % 2 == 0;
+            counter.setInput(Counter4.PIN_CLK, clk);
+            if(i<print || i>=(max-print)) {
+                System.out.println(i + " " + counter.fullTxt() + " " + LocalDateTime.now());
+            }
+        }
+        System.out.println(LocalDateTime.now());
+
 
         System.out.println("END");
     }
