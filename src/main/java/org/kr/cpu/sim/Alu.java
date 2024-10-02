ee4a7715ed2eb937ab26bea90c36f20df8c455ad
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-10-02 13:38:47 +0200

    test Alu with Inc and Dec operations using random generated truth table

--- src/main/java/org/kr/cpu/sim/Alu.java
+++ src/main/java/org/kr/cpu/sim/Alu.java
@@ -10,9 +10,18 @@ public class Alu extends Component {
     private final SignFlipper16 signFlipper = new SignFlipper16("SF");
     private final LineDriver16 driverAdd = new LineDriver16("DRADD");
     private final LineDriver16 driverSub = new LineDriver16("DRSUB");
+    private final LineDriver16 driverInc = new LineDriver16("DRINC"); // always 1
+    private final LineDriver16 driverDec = new LineDriver16("DRINC"); // always -1
     private final Adder16 adder = new Adder16("AD");
 
-    public Alu(String id)  { super(id, new boolean[4+2*16], new boolean[16]); }
+    public Alu(String id)  {
+        super(id, new boolean[4+2*16], new boolean[16]);
+        for(int i=0; i<16; i++) {
+            driverInc.setInput(LineDriver16.PIN_A[i].order, false);
+            driverDec.setInput(LineDriver16.PIN_A[i].order, true);
+        }
+        driverInc.setInput(LineDriver16.PIN_A[0].order, true); // do not forget to set 1
+    }
 
     private void updateOutput() {
         // update operation decoder
@@ -26,12 +35,15 @@ public class Alu extends Component {
         for(int i=0; i<16; i++) driverAdd.setInput(LineDriver16.PIN_A[i].order, input[PIN_B[i].order]);
         driverSub.setInput(LineDriver16.PIN_EN.order, decoder.getOutput(Decoder416.PIN_Q[9].order)); // SUB - 9
         for(int i=0; i<16; i++) driverSub.setInput(LineDriver16.PIN_A[i].order, signFlipper.getOutput(SignFlipper16.PIN_Y[i].order));
+        driverInc.setInput(LineDriver16.PIN_EN.order, decoder.getOutput(Decoder416.PIN_Q[10].order)); // INC - 10
+        driverDec.setInput(LineDriver16.PIN_EN.order, decoder.getOutput(Decoder416.PIN_Q[11].order)); // DEC - 11
         // update adder
         adder.setInput(Adder16.PIN_C0, false);
         // select proper driver for B operand
         LineDriver16 driverB =
                 !driverAdd.input[LineDriver16.PIN_EN.order] ? driverAdd :
-                !driverSub.input[LineDriver16.PIN_EN.order] ? driverSub : driverSub;
+                !driverSub.input[LineDriver16.PIN_EN.order] ? driverSub :
+                !driverInc.input[LineDriver16.PIN_EN.order] ? driverInc : driverDec;
         for(int i=0; i<16; i++) {
             adder.setInput(Adder16.PIN_A[i].order, input[PIN_A[i].order]);
             adder.setInput(Adder16.PIN_B[i].order, driverB.getOutput(LineDriver16.PIN_Y[i].order));
