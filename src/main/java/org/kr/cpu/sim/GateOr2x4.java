ff8a6a625c726decd057ce0c799865de594cee24
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-10-20 20:42:45 +0200

    refactor

--- src/main/java/org/kr/cpu/sim/GateOr2x4.java
+++ src/main/java/org/kr/cpu/sim/GateOr2x4.java
@@ -2,11 +2,11 @@ package org.kr.cpu.sim;
 
 //74x4072
 public class GateOr2x4 extends Component {
-    static final InputPin PIN_A[] = initInputPins("A", 0, 2);
-    static final InputPin PIN_B[] = initInputPins("B", 2, 2);
-    static final InputPin PIN_C[] = initInputPins("C", 4, 2);
-    static final InputPin PIN_D[] = initInputPins("D", 6, 2);
-    static final OutputPin PIN_Y[] = initOutputPins("Y", 0, 2);
+    static final InputPin[] PIN_A = initInputPins("A", 0, 2);
+    static final InputPin[] PIN_B = initInputPins("B", 2, 2);
+    static final InputPin[] PIN_C = initInputPins("C", 4, 2);
+    static final InputPin[] PIN_D = initInputPins("D", 6, 2);
+    static final OutputPin[] PIN_Y = initOutputPins("Y", 0, 2);
 
     public GateOr2x4(String id) { super(id, new boolean[8], new boolean[2]);}
 
