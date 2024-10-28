6ed112c8d379234eb0ab9ecb2a540ac99b7c1275
Krzysztof Ruta <krzysztof.ruta@play.pl>
2024-10-28 17:21:39 +0100

    comment, minor refactor

--- src/main/java/org/kr/cpu/sim/Latch8.java
+++ src/main/java/org/kr/cpu/sim/Latch8.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Latch8 extends Component {
+    // OE pin should be checked by components using latch (just like line driver)
     static final InputPin PIN_OE = new InputPin("OE", 0);
     static final InputPin[] PIN_D = initInputPins("D", 1, 8);
     static final InputPin PIN_CLK = new InputPin("CLK", 9);
@@ -12,10 +13,14 @@ public class Latch8 extends Component {
 
     public Latch8(String id) {
         super(id, new boolean[10], new boolean[8]);
-        for(int i=0; i<8; i++) setOutput(PIN_Q[i].order, false);
+        for(int i=0; i<8; i++) {
+            setOutput(PIN_Q[i].order, false);
+            state[i] = false;
+        }
     }
 
-    private final boolean[] state = new boolean[] {false, false, false, false, false, false, false, false};
+    // start with all zeros
+    private final boolean[] state = new boolean[8];
     @Override
     protected void updateOutput() {
         for(int i=0; i<PIN_Q.length; i++) setOutput(PIN_Q[i].order,  state[i]);
@@ -26,7 +31,8 @@ public class Latch8 extends Component {
         boolean prevClk = getInput(PIN_CLK.order);
         boolean clkRisingEdge = pinNo == PIN_CLK.order && value && !prevClk;
         if(clkRisingEdge) {
-            // store state due to rising clock edge (NOTE: clock should be updated AFTER all data lines)
+            // store state due to rising clock edge
+            // NOTE: clock should be updated AFTER all data lines, otherwise changes to data would not be reflected in output
             for(int i=0; i<8; i++) state[i] = getInput(PIN_D[i].order);
         }
         super.setInput(pinNo, value, false);
